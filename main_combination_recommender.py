46f677bb5d9b35c5683c77902d303f76634b2480
Guillaume Letellier <38403444+Guigui14460@users.noreply.github.com>
2021-03-24 22:14:11 +0100

    Delete main_combination_recommender.py

--- main_combination_recommender.py
+++
@@ -1,8 +0,0 @@
-from recommender_system.data_processor import ProcessData
-from recommender_system.recommenders.combination_recommender import CombinationRecommender
-
-
-data = ProcessData.load_model()
-recommender = CombinationRecommender(
-    data, "processed_movies_similarity.h5", "trained_svd_model.pickle", analyze=True)
-print("All useful models : SAVED !")
